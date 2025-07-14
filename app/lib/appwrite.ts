a3b4f7d1c0115af33030261d0ebfb8310adad2d3
peterjosephmwangi <mwangijosephpeter@gmail.com>
2025-07-14 15:48:52 +0300

    updated appwrite configurations

--- app/lib/appwrite.ts
+++ app/lib/appwrite.ts
@@ -1,13 +1,14 @@
+// app/lib/appwrite.ts
 import { Client, Databases, Storage, Account, ID } from "appwrite";
 
 const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1") // Use your Appwrite endpoint if self-hosted
-  .setProject("6807163400296f58b59c" || ""); // Your Project ID
+  .setProject("6807163400296f58b59c"); // Your Project ID, no fallback needed
 
 const databases = new Databases(client);
-const storage = new Storage(client); // ✅ Add this to enable file uploads
+const storage = new Storage(client);
 export const account = new Account(client);
 export { ID };
 
